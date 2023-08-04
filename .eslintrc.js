135bfdc4e79cad3f406d1fa36712c2cc4652fb72
Santiago Rodríguez <srodriguezm95@gmail.com>
2023-08-04 12:20:03 -0500

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    // project: './tsconfig.json'
+    project: './tsconfig.json'
   },
   plugins: [],
   rules: {
